ec0970775c6b14484c3bd58f0ea45047cb953982
Gabriel Onike <onikegabriel@gmail.com>
2020-04-03 17:17:45 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
   if position_taken?
-    true
+    next
   elsif "#{board[index]}".between?("#{board[0]}","#{board[8]}")
     true
   else
